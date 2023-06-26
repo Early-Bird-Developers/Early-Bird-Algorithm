868483f4e543f36b81cade913472706ad24a6287
Monixc <chris13404@gmail.com>
2023-06-26 20:39:21 +0900

    [BOJ]9506 - 완전수 구하기

--- 황다경/단계별 풀이(9) - 약수, 배수와 소수/230626_9506.py
+++ 황다경/단계별 풀이(9) - 약수, 배수와 소수/230626_9506.py
@@ -6,7 +6,7 @@ while True:
     if n == -1:
         break
 
-    for i in range(1,n//2+1):
+    for i in range(1,n//2+1): #n의 자기 자신을 제외한 가장 큰 약수는 n을 2로 나눈 것
         if n % i == 0:
             n_list.append(i)
 
