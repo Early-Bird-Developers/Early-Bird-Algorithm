aa1d0a25788c25f817306c7e91b11901bd9fd3e1
엄혜영 <112528738+EomHyeYeong@users.noreply.github.com>
2024-01-05 23:54:58 +0900

    [programmers] 문자 리스트를 문자열로 변경하기

---
+++ 엄혜영/프로그래머스 코딩테스트/기초+입문 문제/Day 3/문자 리스트를 문자열로 변경하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        
+        for(String k : arr) {
+            answer += k;
+        }
+        
+        return answer;
+    }
+}
