187f7ce380117da8a9f441abd79dbd3d736d96af
엄혜영 <112528738+EomHyeYeong@users.noreply.github.com>
2024-01-06 00:12:12 +0900

    [programmers] 배열의 평균값

---
+++ 엄혜영/프로그래머스 코딩테스트/기초+입문 문제/Day 4/배열의 평균값.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        
+        for(double n : numbers)
+            answer += n;
+        
+        return answer / (double)numbers.length;
+    }
+}
